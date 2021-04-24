43a73bcea96fe7135c52ca84b967421db4226bab
ibrahimaktunc <aktunc_44@hotmail.com>
2021-04-25 01:46:41 +0300

    xml den verileri çekmek için fonksiyon olşturuldu

--- VizeOdev/Form1.cs
+++ VizeOdev/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,7 +16,38 @@ namespace VizeOdev
         public Form1()
         {
             InitializeComponent();
-            int a;
+          
+        }
+        void fetchxmldata()
+        {
+            try
+            {
+                string BASE_URL = "http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/tpc-h/part.xml";
+
+                var xmlDoc = new XmlDocument();
+                xmlDoc.Load(BASE_URL);
+
+                string P_NAME = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_NAME").InnerXml;
+                string P_MFGR = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_MFGR").InnerXml;
+                string P_BRAND = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_BRAND").InnerXml;
+                string P_TYPE = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_TYPE").InnerXml;
+                string P_SIZE = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_SIZE").InnerXml;
+                string P_CONTAINER = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_CONTAINER").InnerXml;
+                string P_RETAILPRICE = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_RETAILPRICE").InnerXml;
+                string P_COMMENT = xmlDoc.SelectSingleNode("table[@ID='part']/T/P_COMMENT").InnerXml;
+                
+                MessageBox.Show(P_NAME+"\n"+P_MFGR+"\n", "baslık", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.CompanyName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fetchxmldata();
         }
     }
 }
